58ed6a55abc2006c0dd0772e98e4b00f61160f9f
Coen van Vollenhoven <coenvanvollenhoven@Coens-MacBook-Air.local>
2022-04-17 19:34:17 +0200

    fixed libft for push swap

---
+++ push_swap/libft/ft_calloc.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_calloc.c                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: dritsema <dritsema@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/10/15 14:41:16 by dritsema      #+#    #+#                 */
+/*   Updated: 2021/10/15 15:18:49 by dritsema      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*ptr;
+
+	ptr = (void *)malloc(count * size);
+	if (ptr)
+		ft_bzero(ptr, count * size);
+	return (ptr);
+}
